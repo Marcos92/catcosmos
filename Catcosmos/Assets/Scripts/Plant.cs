77b84d225aeb491857bc034e1a619403d409c49c
Marcos <marcos.mgbr@gmail.com>
2018-02-24 12:01:53 +0000

    Implement growth stages

---
+++ Catcosmos/Assets/Scripts/Plant.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour 
+{
+	//[HideInInspector]
+	public float age = 0f;
+	
+	public int growthStage = 0;
+
+	private int maxGrowthStage = 2;
+
+	private float[] growthTransitions = {5f, 10f, 15f};
+	
+	void Update () 
+	{
+		if(growthStage < maxGrowthStage)
+		{
+			age += Time.deltaTime;
+
+			if(age >= growthTransitions[growthStage])
+			{
+				growthStage++;
+				transform.localScale *= 1.5f; //Replace later with tree growing animation
+			}
+		}
+	}
+}
